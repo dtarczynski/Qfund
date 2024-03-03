3ab8e5229ed25784c9d73348536b8709a049807a
dtarczynski <dtarczynski@gmail.com>
2024-03-03 22:39:50 +0100

    Restructured .sln Added Wolverine

---
+++ Qfund.Application/Transactions/Queries/GetUserTransactionsQuery.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Hitachi Energy. All rights reserved.
+
+namespace Qfund.Application.Transactions.Queries;
+
+public record GetUserTransactionsQuery(DateOnly From, DateOnly To)
+{
+
+}
